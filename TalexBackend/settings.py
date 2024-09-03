b161a565cd29868587de405dd8597787f4711e47
Gunhee Kim <gunhee2001@snu.ac.kr>
2024-09-03 10:25:38 +0900

    modify installed apps

--- TalexBackend/settings.py
+++ TalexBackend/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'corsheaders',
+    'users',
+    'talents',
+    'challenges',
+    'learning',
+    'marketplace',
+    'community',
 ]
 
 MIDDLEWARE = [
